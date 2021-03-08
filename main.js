e3a2b9608692743b2239e4642f1544a72f5d0eb3
Tohzen <melvin.l.mobley@gmail.com>
2021-03-08 16:51:15 -0500

    finished

--- main.js
+++ main.js
@@ -1,44 +1,121 @@
-function isEvenlyDivisible() {
+function isEvenlyDivisible(num1, num2) {
+  if(num1 % num2 === 0){
+    return true;
+  } else return false;
+  
+  }
+
+
+function halfSquare(num) {
+  let value = 0;
+  value = num ** 2 / 1/2;
+  return value;
 }
 
-function halfSquare() {
+function isLong(str) {
+  if(str.length >= 15) {
+    return true;
+  } else (str.length <15)
+  return false;
 }
 
-function isLong() {
+function exclaim(str) {
+  if (str.indexOf('!') === -1){
+      return str += '!'
+  }
+  if (str.indexOf('!') === str.length -1){
+      return str
+  } else {
+      return str.slice(0 ,str.indexOf('!')+1);
+  }
+}  
+  
+
+
+function countWords(str) {
+  var count = 0;
+  for (var i = 0; i < str.length; i++) {
+      if (str.charAt(i) == " ") {
+          count ++;
+      }
+  }
+  return count + 1;
 }
 
-function exclaim() {
+
+function containsDigit(str) {
+  return /\d/.test(str);
 }
 
-function countWords() {
+function containsLowerCase(str) {
+  return /[a-z]/.test(str);
+ }
+
+function containsUpperCase(str) {
+  return /[A-Z]/.test(str);
 }
 
-function containsDigit() {
+function containsNonAlphanumeric(str) {
+  return /[\W_]+/.test(str);
 }
 
-function containsLowerCase() {
+function containsSpace(str) {
+  return /\s/.test(str);
 }
 
-function containsUpperCase() {
+function digits(num) {
+  arr = []
+  if (num < 0){
+      num *= -1
+  }
+  str = num.toString()
+  for (i = 0; i < str.length; i++){
+      if (str[i] !== '.'){
+          arr.push(str[i] * 1)
+      }
+  }
+  return arr
 }
 
-function containsNonAlphanumeric() {
+function truncate(str) {
+  newStr = "";
+  if (str.length >= 15) {
+      for (i = 0; i <= 7; i++) {
+          newStr += str[i];
+      }
+      newStr;
+      newStr += "...";
+      return newStr;
+  } else {
+      return str;
+  }
 }
 
-function containsSpace() {
+
+function isValidPassword(str) {
+  if (
+      containsUpperCase(str) === true &&
+      containsLowerCase(str) === true &&
+      containsDigit(str) === true &&
+      containsNonAlphanumeric(str) === true &&
+      containsSpace(str) === false
+  ) {
+      return true;
+  } else {
+      return false;
+  }
 }
 
-function digits() {
+function onlyPunchy(arr) {
+  newArr = []
+  for (title of arr){
+      if (exclaim(title).length < 15){
+          newArr.push(exclaim(title))
+      }
+  }
+  return newArr
 }
 
-function truncate() {
-}
-
-function isValidPassword() {
-}
-
-function onlyPunchy() {
-}
 
 
 module.exports = {
